e205b220ddb15b91cabf5c255afb0a3292ddc305
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 20:52:23 +0900

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,47 +1,36 @@
-# 대문
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 basedir = os.path.dirname(os.path.abspath(__file__))
-
-
-from app.services.calculator import CalculatorService
-from app.services.user import UserService
-from app.services.score import ScoreService
-from app.services.grade import GradeService
+from app.api.endpoints.url import Url
+from app.constants.menus import DDARUNG, LOGIN, LOGOUT, CALCULATOR, GRADE, \
+    QUIZ_1, QUIZ_2, QUIZ_3, QUIZ_4, TITANIC
+    
 
 def print_menu():
-    print("0. 전체 프로그램 종료")
-    print("1. 계산기 프로그램")
-    print("2. 로그인 프로그램")
-    print("3. 성적표 프로그램")
-    menu = input("메뉴를 선택하세요 :")
+    print(' ###################')
+    print(f'로그인 : {LOGIN}')
+    print(f'로그아웃 : {LOGOUT}')
+    print(f'계산기 : {CALCULATOR}')
+    print(f'성적표 : {GRADE}') 
+    print(f'따릉이 : {DDARUNG}') 
+    print(f'타이타닉 : {TITANIC}') 
+    print(f'퀴즈 1 : {QUIZ_1}') 
+    print(f'퀴즈 2 : {QUIZ_2}')
+    print(f'퀴즈 3 : {QUIZ_3}')
+    print(f'퀴즈 4 : {QUIZ_4}')
+    menu = input('메뉴에서 URL을 카피해서 입력하시오\n')
+    print(' ###################')
     return menu
-
+    
 def main():
-       
+    url = Url()
     while 1:
         menu = print_menu()
-        if menu == '0' :
-            print("전체 프로그램을 종료합니다")
-            break        
-        elif menu == '1 ':
-            calculatorService = CalculatorService()   
-            first = int(input("첫번째 값 : ")) 
-            second = int(input("두번째 값 : ")) 
-            calculatorService. calculate(first, second)            
-        elif menu == '2':
-            userService = UserService()
-            id = input('id')    
-            password = input('password')
-            userService.login(id, password)            
-        elif menu == '3':
-            gradeService =  GradeService()
-            name = input('이름을 입력하세요: ')    
-            kor, eng, math = map(int, input().split())
-            grade = gradeService.get_grade(name, kor, eng, math)          
-            print(f'이름 : {name} 학점: {grade}')          
-                 
-   
-if __name__  ==  '__main__':
+        if menu == f'{LOGOUT}':
+            break
+        else : url.router(menu)
+                         
+            
+if __name__ == '__main__' :
     main()
